f16d40682529922499401899396b3062b79d5da0
Brayan Suarez <brayan.suarezsegura308@gmail.com>
2025-11-23 22:10:32 -0600

    Se cuadra la importacion de clientes

--- app/Filament/Imports/ClienteImporter.php
+++ app/Filament/Imports/ClienteImporter.php
@@ -7,6 +7,8 @@ use Filament\Actions\Imports\ImportColumn;
 use Filament\Actions\Imports\Importer;
 use Filament\Actions\Imports\Models\Import;
 use Illuminate\Support\Number;
+use App\Models\Ruta;
+use App\Models\User;
 
 class ClienteImporter extends Importer
 {
@@ -16,63 +18,66 @@ class ClienteImporter extends Importer
     {
         return [
             ImportColumn::make('tipo_documento')
-                ->example('CC')                
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->example('CC') 
+                ->validationAttribute('Tipo de documento inválido')               
+                ->rules(['nullable', 'max:255', 'required']),
             ImportColumn::make('numero_documento')
-                    ->example('1234567890')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->example('1234567890', 'unique')
+                ->validationAttribute('Número de documento inválido')
+                ->rules(['nullable', 'max:255', 'required', 'unique:clientes,numero_documento']),
             ImportColumn::make('razon_social')
                 ->example('Empresa Ejemplo S.A.S')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->validationAttribute('Razón social inválida')
+                ->rules(['nullable', 'max:255',]),
             ImportColumn::make('direccion')
                 ->example('Calle 123 #45-67')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->validationAttribute('Dirección inválida')
+                ->rules(['nullable', 'max:255']),
             ImportColumn::make('telefono')
                 ->example('3001234567')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->validationAttribute('Teléfono inválido')
+                ->rules(['nullable', 'max:255', 'unique:clientes,telefono']),
             ImportColumn::make('ciudad')
                 ->example('Bogotá')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->validationAttribute('Ciudad inválida')
+                ->rules(['nullable', 'max:255']),
             ImportColumn::make('email')
-                ->example('example@example.com')
-                ->requiredMapping()
-                ->rules(['required', 'email', 'max:255']),
+                ->example('cliente@email.com')
+                ->validationAttribute('Email inválido')
+                ->rules(['nullable', 'email', 'max:255', 'unique:clientes,email']),
             ImportColumn::make('representante_legal')
                 ->example('Juan Pérez')
-                ->requiredMapping()
-                ->rules(['required', 'max:255']),
+                ->validationAttribute('Representante legal inválido')
+                ->rules(['nullable', 'max:255']),
             ImportColumn::make('activo')
                 ->example('1')
-                ->requiredMapping()
+                ->validationAttribute('Activo inválido')
                 ->boolean()
-                ->rules(['required', 'boolean']),
+                ->rules(['nullable', 'boolean']),
             ImportColumn::make('novedad')
                 ->example('Nuevo')
+                ->validationAttribute('Novedad inválida')
                 ->rules(['max:255']),
             ImportColumn::make('ruta')
-                ->example('Ruta 1')
-                ->relationship(resolveUsing: 'nombre_ruta')
-                ->rules(['max:255']),
-            ImportColumn::make('comercial')                
-                ->example('Juan Comercial')
-                ->relationship(resolveUsing: ['name', 'email']) 
-                ->rules(['max:255']),
+                ->example('Norte')
+                ->relationship(resolveUsing: 'ruta')
+                ->validationAttribute('Ruta Invalida')
+                ->rules(['nullable', 'max:255']),
+            ImportColumn::make('comercial') 
+                ->example('Comercial')
+                ->validationAttribute('Comercial inválido')
+                ->relationship(resolveUsing: 'name')
+                ->rules(['nullable', 'max:255']),
             ImportColumn::make('tipo_cliente')
                 ->example('MAYORISTA')
-                ->requiredMapping()
-                ->rules(['required']),
+                ->validationAttribute('Tipo de cliente inválido')
+                ->rules(['nullable', 'in:ELECTRONICO,REMISIONADO,MAYORISTA,MINORISTA,FERRETERO']),
             ImportColumn::make('rut_imagen')
                 ->rules(['max:255']),
             ImportColumn::make('retenedor_fuente')
                 ->example('SI')
-                ->requiredMapping()
-                ->rules(['required']),
+                ->validationAttribute('Retenedor de fuente inválido')
+                ->rules(['nullable']),
         ];
     }
 
