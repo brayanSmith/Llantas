58ae20c33be5145b13688f87d6de080034761006
unknown <brayan.suarezsegura308@gmail.com>
2026-01-23 02:02:06 -0500

    m

--- resources/views/livewire/p-o-s.blade.php
+++ resources/views/livewire/p-o-s.blade.php
@@ -9,7 +9,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9m5-9v9m6-9v9m2-9l2 9" />
             </svg>
-            <span class="ml-1 font-bold">({{ collect($this->cart)->sum('cantidad') }})</span>
+            <span class="ml-1 font-bold">({{ collect($this->cart)->sum(fn($item) => is_numeric($item['cantidad']) ? (int)$item['cantidad'] : 0) }})</span>
         </button>
 
         <!-- Modal -->
