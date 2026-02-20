f535dd9d7e597fe1225d65a55d82a44c2a0ee37a
unknown <brayan.suarezsegura308@gmail.com>
2026-02-20 18:13:26 -0500

    Se cuadran rolessdasdasds

--- app/Filament/Resources/PedidoEnRutas/Tables/PedidoEnRutasTable.php
+++ app/Filament/Resources/PedidoEnRutas/Tables/PedidoEnRutasTable.php
@@ -36,7 +36,7 @@ class PedidoEnRutasTable
                     ->where('estado_venta', 'VENTA');
 
                 // Si el usuario no es super_admin, mostrar solo sus pedidos
-                if (!auth()->user()->hasRole(['super_admin', 'financiero'])) {
+                if (!auth()->user()->hasRole(['super_admin', 'financiero', 'Financiero', 'Logistica', 'logistica'])) {
                     $query->where('user_id', auth()->id());
                 }
 
